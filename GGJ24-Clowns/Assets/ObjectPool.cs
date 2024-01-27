76b79df9836211a5d60c18f216ad5539a9b851ca
textfox233 <78984659+textfox233@users.noreply.github.com>
2024-01-27 14:12:16 +0000

    Pool in a Working State
    Generic pool class working, can show or hide a specific number of specified objects

--- GGJ24-Clowns/Assets/ObjectPool.cs
+++ GGJ24-Clowns/Assets/ObjectPool.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
-    public static ObjectPool SharedInstance;
+    // public static ObjectPool SharedInstance;
     public List<GameObject> pooledObjects;
     public GameObject objectToPool;
     public int amountToPool;
-    
-    void Awake()
-    {
-        SharedInstance = this;
-    }
 
     // Start is called before the first frame update
     void Start()
--- GGJ24-Clowns/Assets/TestInput.cs
+++ GGJ24-Clowns/Assets/TestInput.cs
@@ -6,6 +6,8 @@ public class TestInput : MonoBehaviour
 {
     private Vector3 _startingPosition = Vector3.zero;
     public float offset;
+    public ObjectPool cubePool;
+    public ObjectPool spherePool;
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +23,13 @@ public class TestInput : MonoBehaviour
             Debug.Log("'1' button down");
             Debug.Log("Pulling a cube from the pool");
 
-            GameObject cube = ObjectPool.SharedInstance.GetPooledObject();
+            // GameObject cube = ObjectPool.SharedInstance.GetPooledObject();
             
+            GameObject cube = cubePool.GetPooledObject();
+
             if (cube == null)
             {
-                Debug.Log("No more inactive objects remaining.");
+                Debug.Log("No more inactive cubes remaining.");
             }
             else
             {
@@ -41,11 +45,11 @@ public class TestInput : MonoBehaviour
             Debug.Log("'2' button down");
             Debug.Log("Returning a cube to the pool");
 
-            GameObject cube = ObjectPool.SharedInstance.GetPooledObject(true);
+            GameObject cube = cubePool.GetPooledObject(true);
 
             if (cube == null)
             {
-                Debug.Log("No more active objects remaining.");
+                Debug.Log("No more active cubes remaining.");
             }
             else
             {
@@ -54,5 +58,46 @@ public class TestInput : MonoBehaviour
             }
         }
 
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Debug.Log("'3' button down");
+            Debug.Log("Pulling a sphere from the pool");
+
+            // GameObject cube = ObjectPool.SharedInstance.GetPooledObject();
+            
+            GameObject sphere = spherePool.GetPooledObject();
+
+            if (sphere == null)
+            {
+                Debug.Log("No more inactive spheres remaining.");
+            }
+            else
+            {
+                sphere.transform.position = _startingPosition;
+                sphere.transform.position += Vector3.up * offset;
+
+                _startingPosition.x += offset;
+            
+                sphere.SetActive(true);
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Debug.Log("'4' button down");
+            Debug.Log("Returning a sphere to the pool");
+
+            GameObject sphere = spherePool.GetPooledObject(true);
+
+            if (sphere == null)
+            {
+                Debug.Log("No more active spheres remaining.");
+            }
+            else
+            {
+                _startingPosition.x -= offset;
+                sphere.SetActive(false);
+            }
+        }
     }
 }
