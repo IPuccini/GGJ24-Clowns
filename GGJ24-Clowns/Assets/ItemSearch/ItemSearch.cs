dcc3609cb238ef8e53cac31452aa9a0e4a81615f
JoeVestus <60143310+JoeVestus@users.noreply.github.com>
2024-01-27 15:16:24 +0000

    V2 items transition

--- GGJ24-Clowns/Assets/ItemSearch/ItemSearch.cs
+++ GGJ24-Clowns/Assets/ItemSearch/ItemSearch.cs
@@ -11,12 +11,16 @@ public class ItemSearch : MonoBehaviour
     public Material material2;//ShaderMat
     Renderer rend;
 
+    public bool allowRollover = true;
+
     private void Start()
     {
     }
 
     public void onRollover()
     {
+        if (!allowRollover) return;
+
         Debug.Log("RO");
         transform.DOScaleX(2.2f, 0.6f).SetEase(Ease.OutSine);
         transform.DOScaleY(2.2f, 0.6f).SetEase(Ease.OutSine);
@@ -25,8 +29,10 @@ public class ItemSearch : MonoBehaviour
 
     public void onRolloverOff()
     {
+        if (!allowRollover) return;
+
         Debug.Log("RO");
-        
+
         transform.DOScaleX(2.0f, 0.6f).SetEase(Ease.OutBounce);
         transform.DOScaleY(2.0f, 0.6f).SetEase(Ease.OutBounce);
 
@@ -34,16 +40,29 @@ public class ItemSearch : MonoBehaviour
 
     public void onClickShaderAnim()
     {
+        if (!allowRollover) return;
+
+        allowRollover = false;
+
         Debug.Log("RO");
-        transform.DOScaleX(2.4f, 0.6f).SetEase(Ease.OutBack);
+        transform.DOScaleX(2.4f, 0.6f).SetEase(Ease.OutBack).OnComplete(()=> {
+            rend = GetComponent<Renderer>();
+            rend.material = material2;
+            transform.DOScaleX(2.4f, 2.5f).OnComplete(() =>
+            {
+                //rend.material = Empty;
+                //rend.material.color.a = 0;
+                rend.enabled = false;
+                
+            });
+        });
         transform.DOScaleY(2.4f, 0.6f).SetEase(Ease.OutBack);
 
-        rend = GetComponent<Renderer>();
-        rend.material = material2;
-        
+        //rend = GetComponent<Renderer>();
+        //rend.material = material2;
+
         //Destroy(material2);
 
     }
-
 
 }
